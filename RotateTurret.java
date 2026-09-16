327da8ea2732ad3034e88332961f8b669bc2e2f3
Mikeplaysgenshin <10033191@mcpsmd.net>
2026-09-16 10:27:57 -0400

    add reset rotation to teleOp and add accessors to heading and status

--- RotateTurret.java
+++ RotateTurret.java
@@ -7,14 +7,15 @@ public class RotateTurret{
   //sensitivity=desired degreePerSecond/ticks/degreePerSecond at full power
   public double sensitivity;
   public double power;
-  public int resetRotation;
+  public boolean resetRotation;
+  public boolean isRotating;
   public RotateTurret(HardwareMap hardwareMap) {
     RotatingTurret = hardwareMap.get(DcMotorEx.class, "RotatingTurret");
   }
   public init(){
     currentHeading=0;
   }
-    //autonoumus  
+    //autonomous  
     public void AutoRotate(Double degrees){
       
       currentHeading+=degrees;
@@ -25,39 +26,63 @@ public class RotateTurret{
       //prevent rotating too much by setting boundary at 190/170 degrees
       if(degrees>0&&currentHeading>190){
         degrees-=360;
-        resetrotation=1;
+        resetrotation=true;
       }else if(degrees<0&&currentHeading<170){
         degrees+=360;
-        resetRotation=1;
+        resetRotation=true;
       }
-      if(resetRotation==0){
       if(Double degrees>0){
       RotatingTurret.setPower(1);
+        isRotating=true;
         //letting the DcmotorMove
         sleep(Math.abs(degrees)/degreePerSecond*1000);
         RotatingTurret.setPower(0);
+        isRotating=false;
     }else{
         RotatingTurret.setPower(-1);
+        isRotating=true;
         sleep(Math.abs(degrees)/degreePerSecond*1000);
         RotatingTurret.setPower(0);
+        isRotating=false;
       }
-      }else{
-       if(Double degrees>0){
-         RotatingTurret.setPower(1);
-       }else{
-       }
-      }
+   resetRotation=false;
     }
   //teleOp
       public void TeleRotate(float magnitude,int direction){
-        if(currentHeading>-360){
+        if(currentHeading>360){
         currentHeading-=360;
       }
-        power=magnitude*sensitivity*direction;
-        RotatingTurret.setPower(magnitude*sensitivity*direction);
         //update heading
         currentheading+=ticksPerSecond*power*degreePerSecond;
-        //if exceeds target of 190/170,rotate back
-        if(currentheading>190&&)
-      }
+        power=magnitude*sensitivity*direction;
+        //if exceeds target of 190 forward/170 backward,rotate back
+        if(currentheading>190&&direction==1){
+          resetRotation=true;
+          RotatingTurret.setPower(-1);
+          isRotating=true;
+          sleep(360/degreePerSecond*1000); 
+          RotatingTurret.setPower(0);
+          isRotating=false;
+      }else if(currentheading<170&&direction==-1){
+          resetRotation=true;
+          RotatingTurret.setPower(1);
+          isRotating=true;
+          sleep(360/degreePerSecond*1000); 
+          RotatingTurret.setPower(0);
+          isRotating=false;
+        }else{
+          RotatingTurret.setPower(magnitude*sensitivity*direction);
+        }
+        resetRotation=false;
+}
+        //acessors
+        public double GetHeading(){
+          return currentHeading;
+        }
+        public boolean GetresetRotation(){
+          reutrn resetRotation;
+        }
+        public boolean GetIsRotating(){
+          reutrn isRotating;
+        }
 }
